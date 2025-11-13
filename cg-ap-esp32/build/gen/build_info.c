72c7047d638d1a6169dd6e2c1b091cdf9a3d5cb5
galeorhinus <galeorhinus@gmail.com>
2025-11-13 15:35:20 -0600

    ignore some folder

--- cg-ap-esp32/build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20251111-023315";
-const char *build_timestamp = "2025-11-11T02:33:15Z";
-const char *build_version = "1.0";
--- cg-ap-esp32/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20251111-023315/2.20.0-79-gaa4a2491-master";
-const char *mg_build_timestamp = "2025-11-11T02:33:15Z";
-const char *mg_build_version = "202511101631";
